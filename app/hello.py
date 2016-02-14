53a5d4d3cca4a362df17cb4d98a51b6117827527
ykifle <ykifle2@gmail.com>
2016-02-13 22:45:47 -0800

    Add map data

--- app/hello.py
+++ app/hello.py
@@ -11,7 +11,20 @@ def hello():
 @app.route("/mapdata")
 def mapdata():
   data = { "count": 10785236,
-   "photos": []};
+   "photos": [
+      {"photo_id": 27932,
+      "photo_title": "Atardecer en Embalse",
+      "photo_url": "http://www.panoramio.com/photo/27932",
+      "photo_file_url": "http://mw2.google.com/mw-panoramio/photos/medium/27932.jpg",
+      "longitude": -64.404945,
+      "latitude": -32.202924,
+      "width": 500,
+      "height": 375,
+      "upload_date": "25 June 2006",
+      "owner_id": 4483,
+      "owner_name": "Miguel Coranti",
+      "owner_url": "http://www.panoramio.com/user/4483"}
+  ]};
   return flask.jsonify(**data)
 
 if __name__ == "__main__":
