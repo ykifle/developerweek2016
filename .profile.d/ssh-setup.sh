73f5f4bcec8cb465c2ea7530cfb06ad40292091e
ykifle <ykifle2@gmail.com>
2016-02-14 01:15:04 -0800

    Add ssh keys

---
+++ .profile.d/ssh-setup.sh
@@ -0,0 +1,19 @@
+# file: .profile.d/ssh-setup.sh
+
+#!/bin/bash
+echo $0: creating private key files
+
+# Create the .ssh directory
+mkdir -p ${HOME}/.ssh
+chmod 700 ${HOME}/.ssh
+
+# Create the public and private key files from the environment variables.
+echo "${OREGON_PRIVATE_KEY}" > ${HOME}/.ssh/oregon.pem
+chmod 644 ${HOME}/.ssh/oregon.pem
+ssh-add ${HOME}/.ssh/oregon.pem
+
+# Note use of double quotes, required to preserve newlines
+echo "${DEVWEEK_PRIVATE_KEY}" > ${HOME}/.ssh/devweek2016.pem
+chmod 644 ${HOME}/.ssh/devweek2016.pem
+ssh-add ${HOME}/.ssh/devweek2016.pem
+
