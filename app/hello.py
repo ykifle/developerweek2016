c1ee7e373b48cc672e3ab4ca472059b4cf5bd354
ykifle <ykifle2@gmail.com>
2016-02-14 02:00:07 -0800

    Add cmd endpoint

--- app/hello.py
+++ app/hello.py
@@ -38,6 +38,16 @@ def runcommand():
   proc = subprocess.Popen(com, shell=True, stdout=subprocess.PIPE)
   return proc.stdout.read()
 
+@app.route("/check")
+def runcommand():
+  com1 = "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ec2-user@ec2-52-72-154-92.compute-1.amazonaws.com 'sh check.sh'"
+  com2 = "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ec2-user@ec2-52-25-12-43.us-west-2.compute.amazonaws.com 'sh check.sh'"
+  proc1 = subprocess.Popen(com1, shell=True, stdout=subprocess.PIPE)
+  result1 = proc1.stdout.read()
+  proc2 = subprocess.Popen(com2, shell=True, stdout=subprocess.PIPE)
+  result2 = proc2.stdout.read()
+  return "{}\n{}".format(result1, result2)
+
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
   app.run(host='0.0.0.0', port=port)
